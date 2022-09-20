4fc72e1af203ec2139022fc05ba5d6c92911fa5a
Matthew Jack <mtjack@outlook.com>
2022-09-20 02:33:36 +0100

    classname fix

--- reflective-log/components/footer.js
+++ reflective-log/components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer class="flex justify-center bg-slate-600 p-4">
+        <footer className="flex justify-center bg-slate-600 p-4">
             <p>Built by Matthew Jack</p>
         </footer>
     )
